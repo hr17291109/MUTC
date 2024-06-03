4c692847c94fedb37d357f10d7d4c983ef3ef88c
Haruka Nitta <113890890+hr17291109@users.noreply.github.com>
2024-06-03 16:49:49 +0900

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -13,51 +13,31 @@ concatのオプション
 ⇒FLAGSの列の型の不一致を直す
 ⇒列が勝手に動く原因
 """
-# def read_file():
-#     '''
-#     return concat data
-    
-#     '''
-#     # a = glob.glob('twins_model_left/*.csv',recursive = True)
-#     # path_list = glob.glob("twins_model_left/*.csv")
-
-#     a = glob.glob('*.csv',recursive = True)
-#     path_list = glob.glob("*.csv")
-
-#     name_list = []
-#     for n in range(250):
-#         file = os.path.basename(path_list[n+250])
-#         name, ext = os.path.splitext(file)
-#         name_list.append(file)
-#     _data=pd.DataFrame()
-#     for file,name in zip(tqdm.tqdm(path_list[250:500]),name_list):
-#         tmp=pd.read_csv(file,dtype = {'BMY_AIR_TEMP_OPERATIONAL_FLAGS' : object, 'BPY_AIR_TEMP_OPERATIONAL_FLAGS': object, 'WS_OPERATIONAL_FLAGS' : object, 'LTST' : object },usecols=lambda x: x not in ["SCLK", "AOBT"])
-#         _data=pd.concat([_data,tmp],sort=True)
-#     return _data
-
 def read_file():
     '''
     return concat data
     
     '''
-    # a = glob.glob('twins_model_left/*.csv',recursive = True)
-    # path_list = glob.glob("twins_model_left/*.csv")
 
-    a = glob.glob('*.csv',recursive = True)
+    a = glob.glob('*.csv',　recursive = True)
     path_list = glob.glob("*.csv")
 
     name_list = []
-    for n in range(10):
-        file = os.path.basename(path_list[n+10])
+    for n in range(250):
+        file = os.path.basename(path_list[n+250])
         name, ext = os.path.splitext(file)
         name_list.append(file)
     _data=pd.DataFrame()
-    for file,name in zip(tqdm.tqdm(path_list[0:10]),name_list):
-        tmp=pd.read_csv(file, usecols=lambda x: x not in ["SCLK", "AOBT"])
+    for file,name in zip(tqdm.tqdm(path_list[250:500]),name_list):
+        tmp=pd.read_csv(file,dtype = {'BMY_AIR_TEMP_OPERATIONAL_FLAGS' : object, 'BPY_AIR_TEMP_OPERATIONAL_FLAGS': object, 'WS_OPERATIONAL_FLAGS' : object, 'LTST' : object },usecols=lambda x: x not in ["SCLK", "AOBT"])
         _data=pd.concat([_data,tmp],sort=True)
     return _data
 
 def choice_data(df):
+    '''
+    remove unnecessary data
+    
+    '''
     df1 = df[df['BMY_AIR_TEMP_OPERATIONAL_FLAGS'].str.startswith('1101',na = False) |
              df['BMY_AIR_TEMP_OPERATIONAL_FLAGS'].str.startswith('1110',na = False) | 
              df['BMY_AIR_TEMP_OPERATIONAL_FLAGS'].str.startswith('1111',na = False) |
